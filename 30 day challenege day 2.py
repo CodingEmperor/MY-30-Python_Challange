a387b504daa64e869df7fb9cefe4dd42ca548ffe
CodingEmperor <sharukh_i@hotmail.co.uk>
2026-08-10 01:28:08 +0200

    Day 2: My progress

---
+++ 30 day challenege day 2.py
@@ -0,0 +1,40 @@
+
+
+
+import sha
+
+
+first_name = 'Sha'
+last_name = 'shaki'
+country = 'switzerland'
+age = 4000
+is_married = True
+
+
+skills = ['Python', 'JavaScript', 'HTML', 'CSS', 'React', 'Node', 'MongoDB', 'SQL']
+person_info = {
+    'first_name': first_name,
+    'last_name': last_name,
+    'country': country,
+    'age': age,
+    'is_married': True,
+}
+
+# now printing the values stored in the variables
+print('First Name:', first_name)
+print('Last Name:', last_name)
+print('Country:', country)
+print('Age:', age)
+print('Is Married:', is_married)
+print('Skills:', skills)
+print('Person Info:', person_info)
+
+
+# Declaring multiple variables in one line
+first_name, last_name, country, age, is_married = 'Sha', 'shaki', 'switzerland', 4000, True
+
+print('First Name:', first_name)
+print('Last Name:', last_name)
+print('Country:', country)
+print('Age:', age)
+print('Is Married:', is_married)
